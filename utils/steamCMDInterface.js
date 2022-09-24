721b3e5658193772bf0eb8797b46ccc9bab95e7b
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2022-09-24 17:54:04 +0200

    Trial and error with linux environment 7

--- utils/steamCMDInterface.js
+++ utils/steamCMDInterface.js
@@ -46,14 +46,13 @@ export class SteamCMDInterface {
       if (this.platform === "linux") {
         console.log("linux detected, executing ls -la...")
         const output = await exec("ls", ["-la"])
-        console.log(output.toString())
-        console.log(`Running steamCMD like this: ${this.cmd} ${["+login", "anonymous", "+workshop_download_item", gameId, workshopId, "+quit"].join(" ")}`)
+        console.log("Output of ls -la", output.toString())
+        console.log(`Running steamCMD like this: ./${this.cmd} ${["+login", "anonymous", "+workshop_download_item", gameId, workshopId, "+quit"].join(" ")}`)
         const output2 = await exec("ls", ["-r"])
-        console.log(output2.toString())
-        const output3 = await exec("ls", ["-dR"])
-        console.log(output3.toString())
-        const output4 = await exec("find", ["./", "-name", "steamcmd.sh"])
-        console.log(output4.toString())
+        console.log("Output of ls -r", output2.toString())
+        const output3 = await exec("find", ["./", "-name", "steamcmd.sh"])
+        console.log("Output of find ./ -name steamcmd.sh", output3.toString())
+        console.log("================")
       }
       await exec(`./${this.cmd}`, ["+login", "anonymous", "+workshop_download_item", gameId, workshopId, "+quit"]);
 
