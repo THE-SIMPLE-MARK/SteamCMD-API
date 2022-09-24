9652c2bf3f91652da3c999606cd7a0f13cb644af
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2022-09-24 17:33:10 +0200

    Trial and error with linux environment 2

--- utils/steamCMDInterface.js
+++ utils/steamCMDInterface.js
@@ -48,6 +48,8 @@ export class SteamCMDInterface {
         const output = await exec("ls", ["-la"])
         console.log(output.toString())
         console.log(`Running steamCMD like this: ${this.cmd} ${["+login", "anonymous", "+workshop_download_item", gameId, workshopId, "+quit"].join(" ")}`)
+        const output2 = await exec("tree")
+        console.log(output2.toString())
       }
       await exec(`${this.cmd}`, ["+login", "anonymous", "+workshop_download_item", gameId, workshopId, "+quit"]);
 
