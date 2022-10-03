d7012328756f9a8561f000fc5242eade1836a655
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2022-10-03 17:32:40 +0200

    Move all parsing and validations to this project to avoid sending a lot of data to the main project

--- pages/api/getWorkshopCreation.js
+++ pages/api/getWorkshopCreation.js
@@ -7,11 +7,13 @@ import path from "path";
 import fetch, { FormData } from "node-fetch";
 import { fileURLToPath } from "url";
 import { waitForFile } from "/utils/steamCMDUtils";
+import { workbenchSizes, illegalProperties } from "/utils/definitions";
 
 const fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileName);
 
 async function handler(req, res) {
+  if (req.method !== "GET") return res.status(405).send({ message: "Only get can be used in this endpoint." })
   const valid = await authenticateAPIKey(req?.headers?.authorization);
   if (!valid) return res.status(401).send({ message: "API Key is invalid" })
 
@@ -58,12 +60,20 @@ async function handler(req, res) {
     // parse XML to a JS Object
     const parser = new XMLParser({
       ignoreAttributes: false,
-      attributeNamePrefix : "",
+      attributeNamePrefix : "@_",
     });
 
     const vehicleXML = await parser.parse(fileData);
+    console.log("XML parsed")
 
-    res.status(200).send({ message: "OK", vehicleXML })
+    // validate the XML by getting the size of the creation and any performance mods
+    const { dimensionX, dimensionY, dimensionZ, illegalBlocks } = await validateXML(vehicleXML);
+    console.log("Validating XML...")
+
+    // categorize the size by the creation dimension
+    const sizeCategory = categorizeSize(dimensionX, dimensionY, dimensionZ);
+
+    res.status(200).send({ message: "OK", dimensionX, dimensionY, dimensionZ, sizeCategory, illegalBlocks })
   } catch (err) {
     if (err === "Timeout reached") {
       res.status(500).send({ message: "Waiting for vehicle file timed out" })
@@ -84,3 +94,96 @@ export const config = {
 }
 
 export default withSentry(handler);
+
+
+function categorizeSize(dimX, dimY, dimZ) {
+  console.log(`Dimensions: X: ${dimX} Y: ${dimY} Z: ${dimZ}`)
+  // use the workbenchSizes object to find the smallest workbench size that fits the dimensions
+  if (dimX <= workbenchSizes.TRAIN_DEPOT.dimensionX && dimY <= workbenchSizes.TRAIN_DEPOT.dimensionY && dimZ <= workbenchSizes.TRAIN_DEPOT.dimensionZ) return "TRAIN_DEPOT"
+  if (dimX <= workbenchSizes.SMALL_DOCK.dimensionX && dimY <= workbenchSizes.SMALL_DOCK.dimensionY && dimZ <= workbenchSizes.SMALL_DOCK.dimensionZ) return "SMALL_DOCK"
+  if (dimX <= workbenchSizes.SMALL_HANGAR.dimensionX && dimY <= workbenchSizes.SMALL_HANGAR.dimensionY && dimZ <= workbenchSizes.SMALL_HANGAR.dimensionZ) return "SMALL_HANGAR"
+  if (dimX <= workbenchSizes.MEDIUM_HANGAR.dimensionX && dimY <= workbenchSizes.MEDIUM_HANGAR.dimensionY && dimZ <= workbenchSizes.MEDIUM_HANGAR.dimensionZ) return "MEDIUM_HANGAR"
+  if (dimX <= workbenchSizes.MEDIUM_DOCK.dimensionX && dimY <= workbenchSizes.MEDIUM_DOCK.dimensionY && dimZ <= workbenchSizes.MEDIUM_DOCK.dimensionZ) return "MEDIUM_DOCK"
+  // if bigger than all
+  return "HUGE"
+}
+
+async function validateXML(XMLObj) {
+  const illegalBlocks = []
+
+  let largestX = NaN
+  let largestY = NaN
+  let largestZ = NaN
+
+  let smallestX = NaN
+  let smallestY = NaN
+  let smallestZ = NaN
+
+  let numX = 0
+  let numY = 0
+  let numZ = 0
+
+  function iterate(obj) {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (typeof value === "object") {
+        // recursively iterate through the children if it is an object
+        // also ignore the object and nodes so the node positions do not get confused with the block positions
+        if (key !== "object" && key !== "nodes") iterate(value)
+      } else {
+        // check if the key can have an illegal value
+        if (illegalProperties.hasOwnProperty(key)) {
+          // check if the value is not in the non-illegal limits
+          const illegalPropertyRules = illegalProperties[key]
+          if (!threshold(value, illegalPropertyRules.min, illegalPropertyRules.max)) {
+            illegalBlocks.push({ property: key, value, expected: { min: illegalPropertyRules.min, max: illegalPropertyRules.max } })
+          }
+        }
+
+        // only run size checks for this key if the value is a number
+        if (!isNaN(value) && (key === "@_x" || key === "@_y" || key === "@_z")) {
+          // run size checks
+          if (key === "@_x") {
+            numX++
+            if (!largestX || value > largestX) largestX = value
+            if (!smallestX || value < smallestX) smallestX = value
+          } else if (key === "@_y") {
+            numY++
+            if (!largestY || value > largestY) largestY = value
+            if (!smallestY || value < smallestY) smallestY = value
+          } else if (key === "@_z") {
+            numZ++
+            if (!largestZ || value > largestZ) largestZ = value
+            if (!smallestZ || value < smallestZ) smallestZ = value
+          }
+        }
+      }
+    })
+  }
+
+  iterate(XMLObj)
+
+  // calculate dimension from the collected values
+  if (!largestX || (numX < numY || numX < numZ) && largestX < 0) largestX = 0
+  if (!smallestX || (numX < numY || numX < numZ) && smallestX > 0) smallestX = 0
+
+  if (!largestY || (numY < numX || numY < numZ) && largestY < 0) largestY = 0
+  if (!smallestY || (numY < numX || numY < numZ) && smallestY > 0) smallestY = 0
+
+  if (!largestZ || (numZ < numX || numZ < numY) && largestZ < 0) largestZ = 0
+  if (!smallestZ || (numZ < numX || numZ < numY) && smallestZ > 0) smallestZ = 0
+
+  const dimensionX = Math.abs(largestX - smallestX) + 1
+  const dimensionY = Math.abs(largestY - smallestY) + 1
+  const dimensionZ = Math.abs(largestZ - smallestZ) + 1
+
+  return {
+    dimensionX,
+    dimensionY,
+    dimensionZ,
+    illegalBlocks
+  }
+}
+
+function threshold(value, min, max) {
+  return value >= min && value <= max;
+}
---
+++ utils/definitions.js
@@ -0,0 +1,45 @@
+export const workbenchSizes = {
+  "TRAIN_DEPOT": {
+    "dimensionX": 97,
+    "dimensionY": 21,
+    "dimensionZ": 17,
+  },
+  "SMALL_DOCK": {
+    "dimensionX": 57,
+    "dimensionY": 27,
+    "dimensionZ": 27,
+  },
+  "SMALL_HANGAR": {
+    "dimensionX": 71,
+    "dimensionY": 21,
+    "dimensionZ": 63,
+  },
+  "MEDIUM_HANGAR": {
+    "dimensionX": 137,
+    "dimensionY": 37,
+    "dimensionZ": 143,
+  },
+  "MEDIUM_DOCK": {
+    "dimensionX": 227,
+    "dimensionY": 43,
+    "dimensionZ": 79,
+  },
+  "HUGE": {
+    "dimensionX": 9999,
+    "dimensionY": 9999,
+    "dimensionZ": 9999,
+  }
+}
+
+export const illegalProperties = {
+  "@_max_force_scalar": { min: 0, max: 1 },
+  "@_gear_ratio": { min: 0, max: 5 },
+  "@_input_velocity": { min: 0, max: 5 },
+  "@_grip_factor": { min: 0, max: 1 },
+  "@_stiffness_factor": { min: 0, max: 1 },
+  "@_coal_fill": { min: 0, max: 1 },
+  "@_throttle_min": { min: 0, max: 1 },
+  "@_throttle_max": { min: 0, max: 1 },
+  "@_max_force_scale": { min: 0, max: 1 },
+  "@_rps_limit": { min: 0, max: 100 },
+}
